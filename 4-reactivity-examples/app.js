923a2caa48feb9f7b2cb2308629d77ede3cea446
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-03-12 17:02:39 +0300

    reactivity

--- 4-reactivity-examples/app.js
+++ 4-reactivity-examples/app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
-    data(){
-
+    data() {
+        return {
+            search: "",
+            itemList: ["elma", "armut", "kiraz", "çilek"],
+            // filteredList: [],
+        };
     },
-    methods:{
-
-    }
+    methods: {
+        searchList() {
+            return this.itemList.filter((i) => i.includes(this.search));
+        },
+    },
+    computed: {
+        filteredList() {
+            return this.itemList.filter((i) => i.includes(this.search));
+        },
+    },
 }).mount("#app");

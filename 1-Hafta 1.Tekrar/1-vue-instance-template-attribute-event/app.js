f574df2ebcb57dd39e81f7cfa2814e61a8b602f8
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-03-13 21:43:00 +0300

    v-bind v-on @click

---
+++ 1-Hafta 1.Tekrar/1-vue-instance-template-attribute-event/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "Bu Vue tarafından gelen bir bilgidir..",
+      content: "lorem ipsum dolor sit amet",
+      hobbies: [1, 2, 3, 4, 5],
+      personal: {
+        name: "Oğuzhan",
+        lname: "Küçükyamaç",
+        phone: "xxx",
+      },
+      details: `<a href="https://www.google.com">Tıklayınıc</a>`,
+      url: "https://www.google.com",
+    };
+  },
+  methods: {
+    tikla(event) {
+      this.title = "değişen title";
+    },
+  },
+}).mount("#app");

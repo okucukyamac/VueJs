9751f53a050fbc57c5a54efb9567e199c6518a48
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-03-19 23:53:15 +0300

    deneme

---
+++ 2-Hafta/2-todo-app/src/components/ListSection copy.vue
@@ -0,0 +1,22 @@
+<template>
+  <TodoList :myData="todoList" @delete-todo-item="deleteItem($event)" />
+  <ResultBar :itemCount="todoList.length" />
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+import ResultBar from "@/components/ResultBar";
+
+export default {
+    props:{
+        todoList:{
+            type:Array,
+            required:true
+        }
+    },
+  components: {
+    TodoList,
+    ResultBar,
+  },
+};
+</script>

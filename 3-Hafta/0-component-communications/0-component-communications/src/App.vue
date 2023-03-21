dbcc45a05636596a5464fe376007fa7df8df0ca9
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-03-21 21:17:45 +0300

    3. hafta

---
+++ 3-Hafta/0-component-communications/0-component-communications/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+      <input type="text" :value="userData"/>
+      <button @click="userData='Yeni Değer...'">Set Data</button>
+
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      userData:null
+    }
+  }
+}
+</script>

1c52ea8a43fbd7982841725337f91e269b5cb00c
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-03-28 23:52:41 +0300

    cmt

---
+++ 4-Hafta/0-vuex-introduction/src/store.js
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state:{
+        person:{
+            name:"Oğuz",
+            lname:"Küçük",
+            age:28
+        },
+        theme:"dark",
+        permissions:[1,2,3,4,5]
+    }
+})
+
+export default store;
